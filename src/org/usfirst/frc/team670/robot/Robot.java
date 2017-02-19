7bca8a17f65b81275a305890e689ae9cae30d05d
Shaylan Dias <shaylandias@gmail.com>
2017-02-19 04:22:52 -0800

    I added Hi

--- src/org/usfirst/frc/team670/robot/Robot.java
+++ src/org/usfirst/frc/team670/robot/Robot.java
@@ -1,5 +1,5 @@
 package org.usfirst.frc.team670.robot;
-
+//hi
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
